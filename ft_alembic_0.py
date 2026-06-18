235835434db8bd5338e9d096c913f1a32ca9d715
mgerard <mgerard@student.42belgium.be>
2026-06-18 08:56:40 +0200

    add print testing meesga ein all exos

--- ft_alembic_0.py
+++ ft_alembic_0.py
@@ -1,3 +1,6 @@
 import elements
 
+print("=== Alembic 0 ===")
+print("Using: 'import ...' structure to access elements.py")
+print("Testing create_fire: ", end="")
 elements.create_fire()
--- ft_alembic_2.py
+++ ft_alembic_2.py
@@ -1,3 +1,6 @@
 import alchemy.elements
 
+print("=== Alembic 2 ===")
+print("Accessing alchemy/elements.py using 'import ...' structure")
+print("Testing create_earth: ", end="")
 alchemy.elements.create_earth()
